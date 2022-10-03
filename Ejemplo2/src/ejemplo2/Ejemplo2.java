d5bb7007d6d04cd25617dfba78fc5240bb20e1d8
Laura Cuevas <111664627+LauuCL@users.noreply.github.com>
2022-10-02 20:25:49 -0600

    Add files via upload

---
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author labc205
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String mensaje = "Estoy aprendiendo a programar en Java";
+        String ms = "Esto es genial";
+        System.out.print("Hola mundo\ndesde Java\n");
+        System.out.println("Bienvenidos a UAM");
+        
+        System.out.println(mensaje);
+        System.out.println(ms);
+        
+        int nota = 100;
+        System.out.println("Nota = "+nota);
+        float notafinal = (float) 59.9;
+        System.out.println("La nota final es = "+notafinal);
+        double precio = 99.99;
+        System.out.println(precio);
+        boolean presente = true;
+        System.out.println(presente);
+    }
+    
+}

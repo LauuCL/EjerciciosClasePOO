1344a6207679d56ed20e7b2dffe186ae3b121fad
Laura Cuevas <111664627+LauuCL@users.noreply.github.com>
2022-10-02 20:23:58 -0600

    Add files via upload

---
+++ Arreglo/src/arreglo/Arreglo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arreglo;
+
+/**
+ *
+ * @author labc205
+ */
+public class Arreglo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int arreglo[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arreglo[i] = i * 2;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arreglo[i]);
+        }
+
+/*otra forma
+for(int i: arreglo){
+System.out.println(arreglo[i]);
+}
+*/
+    }
+
+}

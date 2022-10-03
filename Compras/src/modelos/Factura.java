0c79ae6e959335261c8f6889f2633ca0924edfe7
Laura Cuevas <111664627+LauuCL@users.noreply.github.com>
2022-10-02 20:25:00 -0600

    Add files via upload

---
+++ Compras/src/modelos/Factura.java
@@ -0,0 +1,37 @@
+package modelos;
+
+import java.util.Date;
+
+/**
+ * @author labc205
+ * @version 1.0
+ * @created 27-sep.-2022 09:24:32
+ */
+public class Factura {
+
+    private Date fechaFact;
+    private int idFact;
+    public Cliente m_Cliente;
+    public Vendedor m_Vendedor;
+    public Carrito m_Carrito;
+
+    public Factura() {
+
+    }
+
+    public void finalize() throws Throwable {
+
+    }
+
+    public double calcIva() {
+        return 0;
+    }
+
+    public double calcMonto() {
+        return 0;
+    }
+
+    public double calcNeto() {
+        return 0;
+    }
+}//end Factura
---
+++ Compras/src/modelos/Vendedor.java
@@ -0,0 +1,21 @@
+package modelos;
+
+/**
+ * @author labc205
+ * @version 1.0
+ * @created 27-sep.-2022 09:24:31
+ */
+public class Vendedor {
+
+    private String apellidosVend;
+    private int codigoVend;
+    private String nombresVend;
+
+    public Vendedor() {
+
+    }
+
+    public void finalize() throws Throwable {
+
+    }
+}//end Vendedor
